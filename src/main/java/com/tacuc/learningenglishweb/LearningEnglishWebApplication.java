a2603cfe0b8fd21cb35e4eb7d2e6fdc3cbd7b1f0
VuongVinh <25023425@vnu.edu.vn>
2026-07-31 13:57:56 +0700

    Khoi tao du an LearningEnglishWeb

---
+++ src/main/java/com/tacuc/learningenglishweb/LearningEnglishWebApplication.java
@@ -0,0 +1,13 @@
+package com.tacuc.learningenglishweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningEnglishWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningEnglishWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tacuc/learningenglishweb/LearningEnglishWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacuc.learningenglishweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningEnglishWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
